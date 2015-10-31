06521dee4598a1787bb7e493f6ed7c9f3460bf05
Frank Celler <frank@arangodb.com>
2015-10-31 01:17:28 +0100

    mongodb additions

--- example.c
+++ example.c
@@ -1,64 +1,49 @@
-#include <stdio.h>
-#include <stdlib.h>
-#include <string.h>
 #include "linenoise.h"
 
+#include <stdio.h>
+#include <string.h>
+#include <stdlib.h>
 
-void completion(const char *buf, linenoiseCompletions *lc) {
-    if (buf[0] == 'h') {
-        linenoiseAddCompletion(lc,"hello");
-        linenoiseAddCompletion(lc,"hello there");
+static const char* examples[] = {
+  "db", "hello", "hallo", "hans", "hansekogge", "seamann", "quetzalcoatl", "quit", "power", NULL
+};
+
+void completionHook (char const* prefix, linenoiseCompletions* lc) {
+  size_t i;
+
+  for (i = 0;  examples[i] != NULL; ++i) {
+    if (strncmp(prefix, examples[i], strlen(prefix)) == 0) {
+      linenoiseAddCompletion(lc, examples[i]);
     }
+  }
 }
 
-int main(int argc, char **argv) {
-    char *line;
-    char *prgname = argv[0];
+int main () {
+  const char* file = "./history";
 
-    /* Parse options, with --multiline we enable multi line editing. */
-    while(argc > 1) {
-        argc--;
-        argv++;
-        if (!strcmp(*argv,"--multiline")) {
-            linenoiseSetMultiLine(1);
-            printf("Multi-line mode enabled.\n");
-        } else if (!strcmp(*argv,"--keycodes")) {
-            linenoisePrintKeyCodes();
-            exit(0);
-        } else {
-            fprintf(stderr, "Usage: %s [--multiline] [--keycodes]\n", prgname);
-            exit(1);
-        }
+  linenoiseHistoryLoad(file);
+  linenoiseSetCompletionCallback(completionHook);
+
+  printf("starting...\n");
+
+  char const* prompt = "linenoise> ";
+
+  while (1) {
+    char* result = linenoise(prompt);
+
+    if (result == NULL) {
+      break;
+    }
+    if (*result == '\0') {
+      free(result);
+      break;
     }
 
-    /* Set the completion callback. This will be called every time the
-     * user uses the <tab> key. */
-    linenoiseSetCompletionCallback(completion);
-
-    /* Load history from file. The history file is just a plain text file
-     * where entries are separated by newlines. */
-    linenoiseHistoryLoad("history.txt"); /* Load the history at startup */
-
-    /* Now this is the main loop of the typical linenoise-based application.
-     * The call to linenoise() will block as long as the user types something
-     * and presses enter.
-     *
-     * The typed string is returned as a malloc() allocated string by
-     * linenoise, so the user needs to free() it. */
-    while((line = linenoise("hello> ")) != NULL) {
-        /* Do something with the string. */
-        if (line[0] != '\0' && line[0] != '/') {
-            printf("echo: '%s'\n", line);
-            linenoiseHistoryAdd(line); /* Add to the history. */
-            linenoiseHistorySave("history.txt"); /* Save the history on disk. */
-        } else if (!strncmp(line,"/historylen",11)) {
-            /* The "/historylen" command will change the history len. */
-            int len = atoi(line+11);
-            linenoiseHistorySetMaxLen(len);
-        } else if (line[0] == '/') {
-            printf("Unreconized command: %s\n", line);
-        }
-        free(line);
-    }
-    return 0;
+    printf("thanks for the input.\n");
+    linenoiseHistoryAdd(result);
+    free(result);
+  }
+    
+  linenoiseHistorySave(file);
+  linenoiseHistoryFree();
 }
