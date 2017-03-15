cdf0059c6ac90659c1a06d420ad63d9485994644
Marcin Konarski <marcin.konarski@codestation.org>
2017-03-15 10:57:05 +0100

    UTF32String: Fix buffer initialization.

--- src/utfstring.hxx
+++ src/utfstring.hxx
@@ -8,38 +8,56 @@
 namespace replxx {
 
 class Utf32String {
- public:
-	Utf32String() : _length(0) { _data = new char32_t[1](); }
+public:
+	Utf32String()
+		: _length( 0 )
+		, _data( nullptr ) {
+		_data = new char32_t[1]();
+	}
 
-	explicit Utf32String(const char* src) {
+	explicit Utf32String(const char* src)
+		: _length( 0 )
+		, _data( nullptr ) {
 		size_t len = strlen(src);
-		_data = new char32_t[len + 1];
+		_data = new char32_t[len + 1]();
 		copyString8to32(_data, len + 1, _length, src);
 	}
 
-	explicit Utf32String(const char8_t* src) {
+	explicit Utf32String(const char8_t* src)
+		: _length( 0 )
+		, _data( nullptr ) {
 		size_t len = strlen(reinterpret_cast<const char*>(src));
-		_data = new char32_t[len + 1];
+		_data = new char32_t[len + 1]();
 		copyString8to32(_data, len + 1, _length, src);
 	}
 
-	explicit Utf32String(const char32_t* src) {
+	explicit Utf32String(const char32_t* src)
+		: _length( 0 )
+		, _data( nullptr ) {
 		for (_length = 0; src[_length] != 0; ++_length) {
 		}
-
 		_data = new char32_t[_length + 1]();
 		memcpy(_data, src, _length * sizeof(char32_t));
 	}
 
-	explicit Utf32String(const char32_t* src, int len) : _length(len) {
+	explicit Utf32String(const char32_t* src, int len)
+		: _length( len )
+		, _data( nullptr ) {
 		_data = new char32_t[len + 1]();
 		memcpy(_data, src, len * sizeof(char32_t));
 	}
 
-	explicit Utf32String(int len) : _length(0) { _data = new char32_t[len](); }
+	explicit Utf32String(int len)
+		: _length( 0 )
+		, _data( nullptr ) {
+		_data = new char32_t[len]();
+	}
 
-	explicit Utf32String(const Utf32String& that) : _length(that._length) {
-		_data = new char32_t[_length + 1]();
+	explicit Utf32String(const Utf32String& that)
+		: _length( 0 )
+		, _data( nullptr ) {
+		_data = new char32_t[that._length + 1]();
+		_length = that._length;
 		memcpy(_data, that._data, sizeof(char32_t) * _length);
 	}
 
@@ -56,7 +74,7 @@ class Utf32String {
 
 	~Utf32String() { delete[] _data; }
 
- public:
+public:
 	char32_t* get() const { return _data; }
 
 	size_t length() const { return _length; }
@@ -81,7 +99,7 @@ class Utf8String {
 	Utf8String(const Utf8String&) = delete;
 	Utf8String& operator=(const Utf8String&) = delete;
 
- public:
+public:
 	explicit Utf8String(const Utf32String& src) {
 		size_t len = src.length() * 4 + 1;
 		_data = new char[len];
@@ -90,10 +108,10 @@ class Utf8String {
 
 	~Utf8String() { delete[] _data; }
 
- public:
+public:
 	char* get() const { return _data; }
 
- private:
+private:
 	char* _data;
 };
 
