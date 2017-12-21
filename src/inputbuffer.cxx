AmokHuginnsson/replxx

commit 3b8f7fe6ef9838533a248a82e62b51b6fa8afb60

Fix prompt display after CTRL+L (clear screen).

src/inputbuffer.cxx
@@ -247,8 +247,8 @@ void InputBuffer::refreshLine(PromptBase& pi) {
 		if (write(1, seq, strlen(seq)) == -1) return;
 	}
 	// position at the end of the prompt, clear to end of screen
-	snprintf(seq, sizeof seq, "\x1b[%dG\x1b[J",
-					 pi.promptIndentation + 1);	// 1-based on VT100
+	snprintf(seq, sizeof seq, "\x1b[%dG\x1b[%c",
+					 pi.promptIndentation + 1, pi.promptCursorRowOffset == 0 ? 'K' : 'J' );	// 1-based on VT100
 	if (write(1, seq, strlen(seq)) == -1) return;
 
 	if ( setup.highlighterCallback ) {

src/replxx.cpp
@@ -254,7 +254,8 @@ void replxx_clear_screen(void) {
 	FillConsoleOutputCharacterA(screenHandle, ' ', inf.dwSize.X * inf.dwSize.Y,
 															coord, &count);
 #else
-	if (write(1, "\x1b[H\x1b[2J", 7) <= 0) return;
+	char const clearCode[] = "\033c\033[H\033[2J\033[0m";
+	if (write(1, clearCode, sizeof ( clearCode ) - 1) <= 0) return;
 #endif
 }
 

commit 3afa6246215b0677c6bde257d2933d6547f61a4b

Clear artifacts left while erasing parts of overlong lines.
Commit: 3b8f7fe6ef9838533a248a82e62b51b6fa8afb60 introduced
bug in cleaning artifacts left after shortening overlong lines.
This commit ammends that.

src/inputbuffer.cxx
@@ -271,8 +271,11 @@ void InputBuffer::refreshLine(PromptBase& pi) {
 		if (write(1, seq, strlen(seq)) == -1) return;
 	}
 	// position at the end of the prompt, clear to end of screen
-	snprintf(seq, sizeof seq, "\x1b[%dG\x1b[%c",
-					 pi.promptIndentation + 1, pi.promptCursorRowOffset == 0 ? 'K' : 'J' );	// 1-based on VT100
+	snprintf(
+		seq, sizeof seq, "\x1b[%dG\x1b[%c",
+		pi.promptIndentation + 1, /* 1-based on VT100 */
+		'J'
+	);
 	if (write(1, seq, strlen(seq)) == -1) return;
 
 	if ( !setup.noColor ) {