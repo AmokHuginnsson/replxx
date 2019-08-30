8d1477b61e1275ed37366a3711e28c67b59f85e3
Marcin Konarski <marcin.konarski@codestation.org>
2019-08-30 07:33:51 +0200

    cmake: Allow building only one of the variants (static vs shared) of the library.

--- build-all.sh
+++ build-all.sh
@@ -19,6 +19,18 @@ while [ ${#} -gt 0 ] ; do
 		continue
 	fi
 
+	if [ \( ${#} -gt 0 \) -a \( "x${1}" = "xstatic-only" \) ] ; then
+		staticOnly="-DBUILD_SHARED_LIBS=OFF"
+		shift
+		continue
+	fi
+
+	if [ \( ${#} -gt 0 \) -a \( "x${1}" = "xshared-only" \) ] ; then
+		sharedOnly="-DBUILD_STATIC_LIBS=OFF -DREPLXX_BuildExamples=OFF"
+		shift
+		continue
+	fi
+
 	if [ ${#} -gt 0 ] ; then
 		installPrefix="-DCMAKE_INSTALL_PREFIX=${1}"
 		shift
@@ -32,13 +44,13 @@ build_target() {
 	cd "build/${target}"
 	if [ "x${msvcxx}" = "x1" ] ; then
 		CMAKE="/cygdrive/c/Program Files/CMake/bin/cmake.exe"
-		"${CMAKE}" -G 'Visual Studio 14' ${installPrefix} ../../
+		"${CMAKE}" ${sharedOnly} ${staticOnly} -G 'Visual Studio 14' ${installPrefix} ../../
 	  "${CMAKE}" --build . --config Debug
 		"${CMAKE}" --build . --config Release
 		"${CMAKE}" --build . --config Debug --target Install
 		"${CMAKE}" --build . --config Release --target Install
 	else
-		cmake -DCMAKE_BUILD_TYPE=${target} ${installPrefix} ../../
+		cmake -DCMAKE_BUILD_TYPE=${target} ${sharedOnly} ${staticOnly} ${installPrefix} ../../
 		make
 		${sudo} make install
 	fi
